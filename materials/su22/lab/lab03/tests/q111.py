c08bc04e0105d19724d94ecbd158bfafd0669ebe
Lingjun Guo <lingjunguo@berkeley.edu>
2022-06-27 22:24:38 -0700

    releasing lab03

---
+++ materials/su22/lab/lab03/tests/q111.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q111',
+    'points': None,
+    'suites': [   {   'cases': [{'code': ">>> string_to_remove\n'hi'", 'hidden': False, 'locked': False}, {'code': ">>> new_word\n'matchmaker'", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
